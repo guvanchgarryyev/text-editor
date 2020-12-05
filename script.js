0e1b1fb4e52d3e6d6c5a8a7bb9e01260a025972f
Guvanch Garryyev <49387249+guvanchgarryyev@users.noreply.github.com>
2020-12-05 14:25:53 -0600

    Uploading already my existing text editor project

---
+++ script.js
@@ -0,0 +1,67 @@
+
+/**
+ * Update the output text as a user types in the textarea
+ * Use the onkeydown function inside HTML
+ */
+function updateText(){
+
+  let text = document.getElementById('text-input').value;
+  document.getElementById('text-output').innerText = text;
+}
+
+/**
+ * Toggle the bold class for the output text
+ * Use the onclick function insite HTML
+ * Look into using this keyword
+ * Use the classList property
+ * Toggle .active class for the button
+ */
+function makeBold(elem){
+  //CODE GOES HERE
+  elem.classList.toggle('active');
+  document.getElementById('text-output').classList.toggle('bold');
+}
+
+/**
+ * Toggle the italic class for the output text
+ */
+function makeItalic(elem){
+  elem.classList.toggle('active');
+  document.getElementById('text-output').classList.toggle('italic');
+}
+
+/**
+ * Toggle the underline class for the output text
+ * Toggle the .active class for the button
+ * Use the classList property
+ * Use contains, remove, and add functions
+ */
+function makeUnderline(elem){
+    //CODE GOES HERE
+    elem.classList.toggle('active');
+    let formatText = document.getElementById('text-output');
+    if(formatText.classList.contains('underline'))
+    {
+      formatText.classList.remove('underline');
+    }
+    else{
+      formatText.classList.add('underline');
+    }
+}
+
+/**
+ * Toggle the style textAlign attribute
+ * Toggle the active state for the align butttons
+ * Use the style property of the element
+ * Make sure to untoggle the active state for all other align buttons
+ */
+function alignText(elem, alignType){
+  document.getElementById('text-output').style.textAlign = alignType;
+
+  let btnList = document.getElementsByClassName('align');
+
+  for(let i = 0; i < btnList.length; i++){
+    btnList[i].classList.remove('active');
+  }
+  elem.classList.toggle('active');  
+}
